e1939e76e6c3eb291a26f4793629b8bce1384feb
Naa Norkor <nortey.sarah@gmail.com>
2022-09-28 15:13:41 +0100

    adds State of main video properties

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import CommentBox from "./components/CommentBox/CommentBox";
 import users from "./assets/Data/video-details.json";
 import DisplayVideoDetails from "./components/DisplayVideoDetails/DisplayVideoDetails";
+import { useState } from "react";
 
 const App = () => {
+const [currentUser, setCurrentUser] = useState (users[0]);
+
+const [currentComment, setCurrentComment] = useState (users[0].comments)
+
+
+
   return (
     <main>
-      <DisplayVideoDetails users={users[0]} />
-      <CommentBox comments={users[0].comments} />
+      <DisplayVideoDetails users={currentUser} />
+      <CommentBox comments={currentComment} />
     </main>
   );
 };
