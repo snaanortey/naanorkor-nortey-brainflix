a81d4726b998fb9e39a1a1ef0066423bcdbc729f
Naa Norkor <nortey.sarah@gmail.com>
2022-10-02 11:47:54 +0100

    deep-dive and fixes

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -1,37 +1,35 @@
 import avatar from "../../assets/Images/Mohan-muruge.jpg";
-import submitIcon from "../../assets/Icons/add_comment.svg"
-import "./CommentForm.scss"
-
+import submitIcon from "../../assets/Icons/add_comment.svg";
+import "./CommentForm.scss";
 
 const CommentForm = () => {
+  return (
+    <form name="newComment" className="form">
+      <div className="form__image-div">
+        <img className="form__image" src={avatar} alt="profile-pic" />
+      </div>
+      <div className="form__content">
+        <h3 className="form__title">JOIN THE CONVERSATION</h3>
 
-    return (
-      <form name="newComment" className="form">
-        <div className="form__image-div">
-          <img className="form__image" src={avatar} alt="profile-pic" />
-        </div>
-        <div className="form__content">
-          <h3 className="form__title">JOIN THE CONVERSATION</h3>
+        <div className="form__template">
+          <input
+            type="text"
+            className="form__input"
+            placeholder="Add a new comment"
+          />
 
-          <div className="form__template">
-            <input
-              type="text"
-              className="form__input"
-              placeholder="Add a new comment"
-            />
-            <button className="form__button" type="submit">
-              <img
-                className="form__button-icon"
-                src={submitIcon}
-                alt="submit icon"
-              ></img>
-              <p className="form__button-text">COMMENT</p>
-            </button>
-          </div>
+          <button className="form__button" type="submit">
+            <img
+              className="form__button-icon"
+              src={submitIcon}
+              alt="submit icon"
+            ></img>
+            <p className="form__button-text">COMMENT</p>
+          </button>
         </div>
-      </form>
-      
-    );
-}
+      </div>
+    </form>
+  );
+};
 
 export default CommentForm;
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,7 @@
+import {DateTime} from "luxon";
 
 export const formatTimestamp = (timestamp) => {
-  const date = new Date(timestamp).toLocaleDateString("en-US", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
-  return date;
+  const humanizedDate = DateTime.fromMillis(timestamp).toRelative();
+  return humanizedDate;
 };
 
